b314c59cd8d4c3e15832ef07d261b4711a8c5070
rubo77 <github@r.z11.de>
2017-09-05 20:43:38 +0200

    sort output file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nodes-bind
+
+Erstellt aus nodes.json ein Zonefile das included werden kann z.B. mit
+
+    $INCLUDE /etc/bind/zones/ffki/db.knoten.ffki
+
+Siehe https://github.com/rubo77/ffki-zone/blob/master/db.ffki#L201
+    
--- createdb.sh
+++ createdb.sh
@@ -2,7 +2,8 @@
 
 TMP="tempdir"
 NODES="https://mesh.freifunk.in-kiel.de/nodes.json"
-TLD=knoten.ffki
+TLD=ffki
+NODESUB=knoten
 
 IsValidHostname () {
   #[a-z] would include umlauts like äöü if LOCALE is set to german
@@ -17,13 +18,18 @@ IsValidHostname () {
 
 mkdir -p $TMP
 cd $TMP
-wget $NODES 
-jq -r '.nodes[]|.nodeinfo|select(.network.addresses|length > 0)|.network.addresses[] +" " +.hostname|.' nodes.json | grep -E "^fda1:384a:74de:4242" > nodelist
-cat nodelist | while read k; do
+wget $NODES -O nodes.json
+jq -r '.nodes[]|.nodeinfo|select(.network.addresses|length > 0)|.network.addresses[] +" " +.hostname|.' nodes.json | grep -E "^fda1:384a:74de:4242" > knotenliste
+echo "; \$ORIGIN $TLD." > db.$NODESUB.$TLD
+echo "" > db.temp
+cat knotenliste | while read k; do
   IP6="$(echo $k | cut -d" " -f1)"
   KNOTEN="$(echo $k | cut -d" " -f2-99|tr _ -|tr . -|tr " " -|tr '[:upper:]' '[:lower:]'|sed 's/ä/ae/;s/ö/oe/;s/ü/ue/;s/ß/ss/g')"
   IsValidHostname "$KNOTEN"
   if [ $? == 0 ]; then
-    echo -e "$KNOTEN.$TLD \t\t\t\t\t86400 \tIN \tAAAA \t$IP6"
+    echo -e "$KNOTEN \t\t\t\t\t86400 \tIN \tAAAA \t$IP6" >> db.temp
   fi
 done
+sort db.temp >> db.$NODESUB.$TLD
+echo "output file:"
+echo "$TMP/db.$NODESUB.$TLD"
